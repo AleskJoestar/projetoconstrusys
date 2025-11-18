eb52fef1607039e67255d7d3776edbfbf5f587a2
Cleverson Dutra <clebera4@gmail.com>
2025-11-18 19:01:30 -0400

    Ajustando Proprietarios
    Criado o Proprietario Testado ok

---
+++ DataContexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api_Construsys.Models;
+
+namespace api_Construsys.DataContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Proprietario> Proprietarios { get; set; }
+    }
+}
--- obj/Debug/net8.0/api_Construsys.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_Construsys.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_Construsys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc12b1b5150d6e13a6977fa7f212a7a8c9c91418")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_Construsys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_Construsys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
